a2909959077cea474a3467f78b68135013dc1f19
SCANGROUP\walhaddad <Wasim.Al-Haddad@squaddigital.com>
2020-01-23 15:33:49 +0300

    home slider

--- js/main.js
+++ js/main.js
@@ -2,5 +2,19 @@ $(document).ready(function () {
 	/*---------- LOAD PARTICLES ----------*/
 	particlesJS.load('particles', 'js/particles-config.json');
 	
+	/*---------- HOME SLIDER ----------*/
+	if($('.home-slider').length){
+		$(".home-slider").owlCarousel({
+			loop: true,
+			autoplay: true,
+			items: 1,
+			nav: false,
+			dots: false,
+			autoplayHoverPause: true,
+			mouseDrag: false,
+			touchDrag: false,
+			animateOut: 'fadeOut'
+		});
+	}
 
 }) //End of document ready
