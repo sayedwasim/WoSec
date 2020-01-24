c1a4076a377b0f95ad224b06d2856ca10aab7377
SCANGROUP\walhaddad <Wasim.Al-Haddad@squaddigital.com>
2020-01-24 12:40:14 +0300

    responsive to ipad

--- js/main.js
+++ js/main.js
@@ -2,14 +2,40 @@ $(document).ready(function () {
 	/*---------- LOAD PARTICLES ----------*/
 	particlesJS.load('particles', 'js/particles-config.json');
 	
+	/*---------- NAVIGATION HAMBURGER ----------*/
+	$(".navigation-hamburger").on("click", function(){
+		if($(this).hasClass('-open')){
+			$(this).removeClass('-open').addClass('-close');
+			$('body').removeClass('menu-open');
+		}
+		else{
+			$(this).removeClass('-close').addClass('-open');
+			$('body').addClass('menu-open');
+		}
+	});
+	
+	/*--- SCROLLING ---*/
+  var $header = $('header');
+  var offset = 70;
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > offset) {
+      $header.addClass('scrolling');
+    } else {
+      $header.removeClass('scrolling');
+    }
+  });
+	
 	/*---------- HOME SLIDER ----------*/
 	if($('.home-slider').length){
 		$(".home-slider").owlCarousel({
 			loop: true,
 			autoplay: true,
+			responsiveRefreshRate:0,
 			items: 1,
 			nav: false,
 			dots: false,
+			margin:0,
 			autoplayHoverPause: true,
 			mouseDrag: false,
 			touchDrag: false,
@@ -27,7 +53,33 @@ $(document).ready(function () {
 			nav: false,
 			dots: false,
 			autoplayHoverPause: true,
-			margin:90
+			margin:90,
+			responsive: {
+        // breakpoint from 0 up
+        0: {
+          items: 1,
+					autoWidth:false
+        },
+        // breakpoint from 570 up
+        570: {
+          items: 1
+        },
+        // breakpoint from 670 up
+        670: {
+          items: 4,
+					autoWidth:false
+        },
+        // breakpoint from 940 up
+        940: {
+          items: 5,
+					margin:60,
+					autoWidth:false					
+        },
+        // breakpoint from 1100 up
+        1100: {
+					autoWidth:true
+        }
+      }
 		});
 	}
 
