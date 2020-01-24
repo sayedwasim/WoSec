b26d1434ee740c344a7838d7126a85060f994495
SCANGROUP\walhaddad <Wasim.Al-Haddad@squaddigital.com>
2020-01-24 09:52:49 +0300

    global stats, partners

--- js/main.js
+++ js/main.js
@@ -16,5 +16,19 @@ $(document).ready(function () {
 			animateOut: 'fadeOut'
 		});
 	}
+	
+	/*---------- PARTNERS SLIDER ----------*/
+	if($('.partners').length){
+		$(".partners").owlCarousel({
+			loop: true,
+			autoplay: true,
+			autoWidth:true,
+			responsiveRefreshRate:0,
+			nav: false,
+			dots: false,
+			autoplayHoverPause: true,
+			margin:90
+		});
+	}
 
 }) //End of document ready
